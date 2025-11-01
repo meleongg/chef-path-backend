de63715498fe47a001ba92f9053f81bc9206814e
Melvin Teo <62491197+meleongg@users.noreply.github.com>
2025-11-01 09:38:47 -0700

    fix: remove SQLite specific args for database connection

--- app/database.py
+++ app/database.py
@@ -9,7 +9,7 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
